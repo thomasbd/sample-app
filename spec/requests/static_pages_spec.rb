ce5c591df712d3fd74dbcc77a41259d602de2ea4
Ben Dowzell <Ben@Dowzell>
2013-08-27 20:12:34 +0100

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,16 +2,26 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
+	let(:base_title) {"Ruby on rails sample app"}
+
   describe "Home page" do
     it "should have the content 'Sample App'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
-    it "should have the right title" do
+
+    it "should have the base title" do
     	visit '/static_pages/home'
-    	expect(page).to have_title("Ruby on rails sample app | Home")
+    	expect(page).to have_title("#{base_title}")
     end
+
+    it "should not  have a custom title" do
+    	visit '/static_pages/home'
+    	expect(page).not_to have_title('| Home') 
+    end
+
+
   end
 
   describe "Help Page" do
@@ -21,7 +31,7 @@ describe "StaticPages" do
   	end
   	it "should have the right title" do
     	visit '/static_pages/help'
-    	expect(page).to have_title("Ruby on rails sample app | Help")
+    	expect(page).to have_title("#{base_title} | Help")
     end
   end
 
@@ -32,11 +42,20 @@ describe "StaticPages" do
   	end
   	it "should have the right title" do
     	visit '/static_pages/about'
-    	expect(page).to have_title("Ruby on rails sample app | About")
+    	expect(page).to have_title("#{base_title} | About")
     end
   end
 
-
+describe "Contact Page" do
+  	it "should have content 'Contact Us'" do
+  		visit '/static_pages/contact'
+  		expect(page).to have_content('Contact Us')
+  	end
+  	it "should have the right title" do
+    	visit '/static_pages/contact'
+    	expect(page).to have_title("#{base_title} | Contact")
+    end
+  end
 
 
 end
